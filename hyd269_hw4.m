3f5beaf09c68feb798c90ca63306cd3843d45573
'Alysa <yoder.alysa@gmail.com>
2018-05-10 17:48:59 -0700

    After input from Tiffany. Changing format. Now using

--- hyd269_hw4.m
+++ hyd269_hw4.m
@@ -15,79 +15,60 @@ data = csvread('FD_input.csv',1,0);
 % Compute G and D matrices, then use them to formulate Ax=b and use
 % a direct solver to find the solution at each time step. 
 % in fully implicit methods, alpha=1
-nnode=data(1);
+nnode=data(1); % get values from csv file
 xnode=data(2);
 ynode=data(3);
+S = data(4); % storativity
+T = data(5); % transmissivity or K because b=1 here
+dt = data(6); % time interval
+dx = data(7); % distance between each node
+wellnode=data(8);
+prate=data(10);
+Ho = data(11); % define initial head 
 tmax=data(12);
-% get values from csv file
-%% manually entering variables, eventually will be file sourced
-% on p. 78 W&A, well is discharging constantly at 2000 m^3 d^-1
+
+
+q = zeros(nnode,1); % flux, this is the boundary condition vector
+DD = zeros(nnode,nnode); % drawdown array (nnode x nnode)
+D = ones(nnode,1); % capacitance array (vector)
+G = ones(nnode,nnode); % conductance array (sparse)
+% hOld is the head at time step n, hNew is nead at n+1
+%% create domain w/numbered nodes
+mesh = reshape(1:nnode, [xnode ynode]);
+%% initialize arrays hOld and hNew
 hOld = zeros(xnode,ynode);
 hNew = zeros(xnode,ynode);
-R = zeros(xnode,ynode);
-DD = zeros(xnode,ynode);
-D = ones(xnode,ynode);
-G = ones(xnode,ynode);
-S = data(4); 
-T = data(5); % on p.78 of the book, T=300 m^2 d^-1
-dt = data(6); 
-dx = data(7);
-Ho = data(11); % define initial head 
-tol=0.001; % may not need this
-% hOld is the head at time step n, hNew is nead at n+1
-%% initialize arrays
-for i=1:23
-    for j=1:23
+for i=1:xnode
+    for j=1:ynode
         hNew(i,j)=Ho; % set hNew and hOld equal to 10 in all cells
         hOld(i,j)=Ho;
-        R(i,j)=0; % set R matrix to 0 in all cells
+        q(i,j)=0; % set R matrix to 0 in all cells
     end
 end
-%% define pumping rate as recharge to cell 
-R(data(8),data(9))=data(10)./dx/dx;
+%% define pumping rate as specified flux BC
+q(wellnode)=prate./dx/dx;
 ti=0:dt:tmax;
-%% dig in: create G and D matrices
-% assume thickness (b) = 1
-% using gwmodfundamentals handout equation 53
-% for i =1:23
-%     for j=1:23
-%         G(i,j) = -T; % from G(i,j) = -T/b * delta x /delta y
-%     end
-% end
-% 
-% for i = 1:23
-%     for j = 1:23
-%         D(i,j) = S.*dx.^2;
-%     end
-% end
-
-% modified from W&A p. 82 figure 4.6
-alpha = 1;
-for it=ti(1:end)
-    for i=2:22
-    for j=2:22
+%% create G matrix
+for i=2:xnode-1
+    for j=2:ynode-1
 h1 = (hOld(i,j+1)+hOld(i,j-1)+hOld(i+1,j)+hOld(i-1,j))/4;
 h2 = (hNew(i,j+1)+hNew(i,j-1)+hNew(i+1,j)+hNew(i-1,j))/4;
 f1 = ((dx.^2).*S)/(4.*T.*dt);
 f2 = 1./(f1+alpha);
 G(i,j) = f2;
-D(i,j) = (f1.*hOld(i,j))+(1-alpha).*(h1-hOld(i,j))+(alpha.*h2)+(R(i,j).*dx.^2)/(4.*T);
+D(i,j) = (f1.*hOld(i,j))+(1-alpha).*(h1-hOld(i,j))+(alpha.*h2)+(q(i,j).*dx.^2)/(4.*T);
     end
 end
 
 % add boundary conditions, these are no flows on all perimeters
-for i = 2:22
+for i = 2:xnode-1
     hNew(i,1)=hNew(i,3);
-    hNew(i,23)=hNew(i,21);
+    hNew(i,ynode)=hNew(i,ynode-2);
 end
-for j = 2:22
+for j = 2:ynode-1
     hNew(1,j)=hNew(3,j);
-    hNew(23,j)=hNew(21,j);
+    hNew(xnode,j)=hNew(xnode-1,j);
 end
-it=it+1;
-end
-% solve for hNew
-plz = G\D; % matrix G currently has no inverse, can't solve yet
 %% Theis analytical solution
 % well is placed in the upper left corner (1,10)
 % verification node is in the center of the grid (5,5)
@@ -123,5 +104,3 @@ set(gca,'XScale','log','YDir','reverse','YScale','log');
 ylabel('Drawdown (m)');
 title('Verification Plot');
 xlabel('Time (d)');
-
-
