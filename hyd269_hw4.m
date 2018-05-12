86b343724b16d0eef6d6a69fa0a737b7b25fdc78
'Alysa <yoder.alysa@gmail.com>
2018-05-12 15:19:44 -0700

    Numerical model now working. Changed some typos from accidental matrix operations to algebraic operations in the G and D matrices. Theis now changed to simpler Jacob solution, still not working properly. Computed drawdown from numerically modeled heads.

--- hyd269_hw4.m
+++ hyd269_hw4.m
@@ -13,18 +13,27 @@ S = data(5); % storativity
 dt = data(6); % time interval
 dx = data(7); % distance between each node, also x sec are because b=1
 wellnode=data(8);
-prate=data(9);
+prate=data(9); %m^3/day
 Ho = data(10); % define initial head 
 tmax=data(11); % max time value
-qp=data(12); % flux at perimter cells, these have gen. head BCs
+qp=data(12); % flux at perimiter cells, these have gen. head BCs
 himag=data(13); % head at imaginary nodes for gen head BC
 Limag=data(14); % distance from node to imaginary nodes
-
 %% create domain w/numbered nodes
 mesh = reshape(1:nnode, [xnode ynode]);
+%% create time vector used for Theis and Direct Solver
+% time=zeros(tmax,1);
+% time_factor = dt; 
+% for i=1:tmax
+% time(1,1)=1;
+% time(1+i,1) = time(i,1)+time_factor;
+% time_factor = 1.2.*(time(i+1)-time(i)); 
+% end
+% time=time./86400;
+time = dt:dt:tmax;
 %% add general head boundary conditions
 q = zeros(nnode,1); % flux, this is the boundary condition vector
-perim = zeros((xnode.*2)+(ynode.*2)-4,1); % perim holds indexes of all perimeter nodes
+perim = zeros((xnode.*2)+((ynode.*2)-4),1); % perim holds indexes of all perimeter nodes
 for i = 1:xnode
     perim(i)=mesh(i,1);
 end
@@ -43,7 +52,7 @@ Gdistal = -(T.*dx)./Limag;
 for w = perim(1:end)
 q(w,1)=qp-((Gdistal).*himag); 
 end
-q(wellnode)=prate./dx/dx; % define pumping rate as specified flux BC
+q(wellnode)=-prate./dx./dx; % define pumping rate as specified flux BC
 %% create G matrix
 % create logical nnode x nnode matrix
 % this will be multiplied by the G value to create a matrix of
@@ -67,10 +76,9 @@ for i=1:nnode
     end
 end
 % populate G matrix with G values at connected nodes and boundary nodes
-Gintra = -(T.*dx)./dx; 
+Gintra = -(.02.*dx)./dx; 
 % Gintra is actually just K because the x sec area is equal to the distance
 % between nodes 
-G = zeros(nnode,nnode); % conductance array (sparse)
 G = cons*Gintra; 
 % now need to overwrite nodes affected by gen head BC with G11'=G11-G10
 Gdiag = eye([nnode nnode])*(Gintra-Gdistal);
@@ -78,46 +86,33 @@ G = G + Gdiag;
 %% create D matrix
 Dval = (dx.^2).*S;
 D = eye([nnode nnode])*Dval; % capacitance array (vector)
-%% initialize arrays hOld and hNew
-hO = ones(nnode,1)*10; % set initial head to 10 m at all nodes
-hOld = ones(nnode,1);
-hNew = ones(nnode,1);
-% hOld is the head at time step n, hNew is nead at n+1
+%% initialize array for storing H values for each node at each time step
+hInit = ones(nnode,1)*Ho; 
+H = zeros(nnode, length(time));
+H(:,1) = hInit; % set initial head to 10 m at all nodes for 1st time step
+%% Format Ax=b matrix to be solved at each time step
+A = (dt*G+D);
+for i = 1:length(time)
+B = D*H(find(H,nnode,'last')) + q*time(i); % selects last column of H
+x = mldivide(A,B);
+H(:,i) = x;
+end
+%% Compute Numerical Drawdown
+numDD = ones(nnode,length(time))*Ho;
+numDD = numDD-H;
 %% Theis analytical solution
-% well is placed in the upper left corner (1,10)
-% verification node is in the center of the grid (5,5)
-% triangle sides are 4 and 5, used pythag to get r
-%r = 6.403124237432849;
-r = 200; % distance from node of interest to well
-Q = 2000; % m^3 d^-1
-uVal = zeros(1,tmax.^2);
-time=zeros(tmax,1);
-for i=1:tmax
-time(i) = time+dt.*1.2;
-end
-for t = 1:length(time)       % create uVal, an array containing u from 1-100, steps of .01
-u = (r.^2.*S)./(4.*T.*time(t));
-uVal(t) = u;
-end
-%% this is the well function evaluated at the u values
-fun = @(uVal) (exp(-uVal(v)))./uVal(v); 
-%% Populating an array with well function values for each time step. 
-WuVal = zeros(1,tmax.^2);
-for v = 1:numel(uVal)      
-Wu = integral(fun,uVal(v),inf, 'arrayValued', true);
-WuVal(v) = Wu; %these make sense w/appendix 1 vals in Fetter
-end
-%% Calculating drawdown at each time step.
-ddVal = zeros(1,tmax.^2);
-for b = 1:numel(WuVal)
-dd = (Q./(4.*pi.*T)).*WuVal(b);
+r = 100; % distance from node of interest to well
+Q = prate; % m^3 d^-1
+ddVal = zeros(length(time),1);
+for b = 1:length(time)
+dd = (prate./(4.*pi.*T)).*log((2.25.*T.*time(b))./((r.^2).*S)); 
 ddVal(b) = dd;
 end
 %% verification plot: drawdown vs. time at a node at least two nodes away
 % from the well node. Include Theis and numerical model results. 
-theis = timeseries(ddVal, time);
-plot(theis);
-set(gca,'XScale','log','YDir','reverse','YScale','log');
+plot(time,ddVal,time,numDD(193,:));
+set(gca,'XScale','log','YDir','reverse');
 ylabel('Drawdown (m)');
 title('Verification Plot');
 xlabel('Time (d)');
+legend('Theis','Numerical')
